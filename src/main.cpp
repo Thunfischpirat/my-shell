7ec1af53a824c4f06e4dd567925cfba05bf762ff
Christian <christian.singer@optonic.com>
2024-12-20 20:55:55 +0100

    Add REPL

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,14 @@ int main() {
   std::cerr << std::unitbuf;
 
   // Uncomment this block to pass the first stage
-  std::cout << "$ ";
-
   std::string input;
-  std::getline(std::cin, input);
+  while (true)
+  {
+    std::cout << "$ ";
 
-  std::cout << input + ": command not found";
+    std::getline(std::cin, input);
+
+    std::cout << input + ": command not found\n";
+  }
  }
 
