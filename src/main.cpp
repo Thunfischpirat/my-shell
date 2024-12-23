899021d3564978e30fc89fe926848f8be1212096
Christian <christian.singer@optonic.com>
2024-12-23 21:50:09 +0100

    Handle PATH variable

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <filesystem>
+#include <cstdlib>
 #include <iostream>
 #include <vector>
 #include <sstream>
@@ -9,11 +11,11 @@ int main() {
   std::cerr << std::unitbuf;
 
   std::vector<std::string> cmds {"exit", "echo", "type"};
+  std::stringstream env_p(std::getenv("PATH"));   
 
   // Uncomment this block to pass the first stage
   std::string input;
-  while (true)
-  {
+  while (true) {
     std::cout << "$ ";
 
     std::getline(std::cin, input);
@@ -29,27 +31,40 @@ int main() {
         return 0;
     }
     else if (tokens[0] == "echo") {
-	int i { 1 };
 	buf = "";
-	while (i < tokens.size()) {
-	  buf += (tokens[i] + " ");
-          i++;
+	for (int i { 1 }; i <= tokens.size(); i++) {
+	   buf += (tokens[i] + " ");
         }
         std::cout << buf << std::endl;
     }
     else if (tokens[0] == "type") {
-      bool found { false };
-      for (int i{ 0 }; i <= tokens.size(); i++) {
-	if (tokens[1] == cmds[i]) {
-           std::cout << tokens[1] << " is a shell builtin" << std::endl;
-	   found = true;
-	   break;
-        }
-      }
-      if (!found)	
-         std::cout << tokens[1] << ": not found" << std::endl;
-    }
-    else
+        bool found { false };
+        for (int i{ 0 }; i <= tokens.size(); i++) {
+          if (tokens[1] == cmds[i]) {
+             std::cout << tokens[1] << " is a shell builtin" << std::endl;
+             found = true;
+             break;
+          }
+	}
+
+	if (found)
+	   continue;
+
+        while(std::getline(env_p, buf, ':')) {
+           if (!std::filesystem::exists(buf))
+	      continue;	
+	   for (auto &p : std::filesystem::directory_iterator(buf)) {
+	      if (p.path().filename() == tokens[1]) {
+	          std::cout << tokens[1] << " is " << p.path() << std::endl;
+	          found = true;
+	          break;
+	      } 
+	   } 
+        } 
+        if (!found)	
+           std::cout << tokens[1] << ": not found" << std::endl;
+    }   
+    else 
        std::cout << input + ": command not found" << std::endl;
   }
  }
